773a6aa257153fe06bb41d2c0b4badc330e40e03
Rabia <rabia@Steels-MacBook-Pro.local>
2018-08-27 23:55:56 +0500

    Convert ContentCard component in pure function

--- src/components/ContentCard.js
+++ src/components/ContentCard.js
@@ -12,42 +12,38 @@ type Props = {|
   songs: Array<Object>,
   selected: ?Object,
 |}
-type State = {||}
 
-export default class ContentCard extends React.Component<Props, State> {
-  render() {
-    const { songs, selected } = this.props
-
-    let songsToShow = songs
-    if (selected) {
-      if (selected.type === 'artist') {
-        songsToShow = songs.filter(item => item.meta && item.meta.artists.includes(selected.identifier))
-      } else if (selected.type === 'genre') {
-        songsToShow = songs.filter(item => item.meta && item.meta.genre && item.meta.genre.includes(selected.identifier))
-      }
+export default (props: Props) => {
+  const { songs, selected } = props
+  let songsToShow = songs
+  if (selected) {
+    if (selected.type === 'artist') {
+      songsToShow = songs.filter(item => item.meta && item.meta.artists.includes(selected.identifier))
+    } else if (selected.type === 'genre') {
+      songsToShow = songs.filter(item => item.meta && item.meta.genre && item.meta.genre.includes(selected.identifier))
     }
-    const songsByAlbums = groupBy(songsToShow, 'meta.album')
-
-    let songsIdsArr = []
-    songsToShow.forEach(song => {
-      songsIdsArr.push(song.id)
-    })
-
-    return (
-      <div className="section-artist" id={selected ? selected.identifier : 'allArtists'}>
-        <div className="space-between section-artist-header">
-          <div>
-            <h2>{selected ? selected.identifier : 'All Artists'}</h2>
-            <p>
-              {Object.keys(songsByAlbums).length} albums, {songsToShow.length} songs
-            </p>
-          </div>
-          <Dropdown songsIds={songsIdsArr} />
-        </div>
-        {Object.keys(songsByAlbums).map(albumName => (
-          <AlbumInfo name={albumName} key={albumName} songs={songsByAlbums[albumName]} />
-        ))}
-      </div>
-    )
   }
+  const songsByAlbums = groupBy(songsToShow, 'meta.album')
+
+  const songsIdsArr = []
+  songsToShow.forEach(song => {
+    songsIdsArr.push(song.id)
+  })
+
+  return (
+    <div className="section-artist" id={selected ? selected.identifier : 'allArtists'}>
+      <div className="space-between section-artist-header">
+        <div>
+          <h2>{selected ? selected.identifier : 'All Artists'}</h2>
+          <p>
+            {Object.keys(songsByAlbums).length} albums, {songsToShow.length} songs
+          </p>
+        </div>
+        <Dropdown songsIds={songsIdsArr} />
+      </div>
+      {Object.keys(songsByAlbums).map(albumName => (
+        <AlbumInfo name={albumName} key={albumName} songs={songsByAlbums[albumName]} />
+      ))}
+    </div>
+  )
 }
