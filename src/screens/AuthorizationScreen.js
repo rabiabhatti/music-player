b2638c37248779788e4101bd4397d166d534fef2
Rabia Iqbal <kaneez.iqbal.b@gmail.com>
2018-09-28 21:17:56 +0500

    Redesign Autherization and Loading screen

--- src/screens/AuthorizationScreen.js
+++ src/screens/AuthorizationScreen.js
@@ -6,8 +6,11 @@ import services from '~/services'
 import connect from '~/common/connect'
 import { authorizeService } from '~/redux/user'
 
-import '~/styles/login.less'
 import type { Service } from '~/services/types'
+
+import flex from '~/less/flex.less'
+import login from '~/less/login.less'
+import button from '~/less/button.less'
 
 type Props = {|
   authorizeService: typeof authorizeService,
@@ -28,9 +31,14 @@ class Login extends React.Component<Props, State> {
 
   render() {
     return (
-      <div className="section-login align-center">
+      <div className={`${login.login} ${flex.align_center}`}>
         {services.map(service => (
-          <button type="button" className="btn-blue-border" onClick={e => this.authorize(service, e)} key={service.name}>
+          <button
+            type="button"
+            key={service.name}
+            onClick={e => this.authorize(service, e)}
+            className={`${button.btn} ${button.btn_blue_border}`}
+          >
             Authorize with {service.name}
           </button>
         ))}
