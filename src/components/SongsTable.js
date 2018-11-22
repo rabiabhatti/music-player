d8a7d8632b4b5882a2b5a756940bbe7e929a4411
rabiasakura <rabia.iqbal.bhatti@gmail.com>
2018-11-22 05:00:36 +0500

    Select songs with and without holding shift key

--- src/components/SongsTable.js
+++ src/components/SongsTable.js
@@ -3,12 +3,14 @@
 import * as React from 'react'
 import connect from '~/common/connect'
 
+import getEventPath from '~/common/getEventPath'
 import { humanizeDuration } from '~/common/songs'
 import { setSongPlaylist, songPlay, songPause } from '~/redux/songs'
 
 import flex from '~/less/flex.less'
 import button from '~/less/button.less'
 import table from '~/less/table.less'
+// import ContextMenu from '~/components/ContextMenu'
 import SongDropdown from '~/components/Dropdown/SongDropdown'
 
 type Props = {|
@@ -20,11 +22,33 @@ type Props = {|
   activeSong: number | null,
 |}
 
-type State = {||}
+type State = {|
+  selected: Array<number>,
+  showContextMenu: boolean,
+|}
 
 class SongsTable extends React.Component<Props, State> {
+  nodes = new Map()
   static defaultProps = {
     playlist: null,
+  }
+
+  state = {
+    selected: [],
+    showContextMenu: false,
+  }
+
+  componentDidMount() {
+    document.addEventListener('click', this.handleClick)
+  }
+  componentWillUnmount() {
+    document.removeEventListener('click', this.handleClick)
+  }
+
+  handleClick = (e: MouseEvent) => {
+    this.nodes.forEach(a => {
+      console.log(getEventPath(e).includes(a))
+    })
   }
 
   playAtIndex = (index: number) => {
@@ -42,7 +66,49 @@ class SongsTable extends React.Component<Props, State> {
     dispatch(songState === 'playing' ? songPause() : songPlay())
   }
 
+  onContextMenu = (song, e) => {
+    e.preventDefault()
+    e.persist()
+    console.log(e.nativeEvent, e)
+    const { showContextMenu } = this.state
+    // const ele = getEventPath(e).includes(this.ref)
+    this.setState({
+      showContextMenu: !showContextMenu,
+    })
+  }
+
+  selectRow = (e, id: number) => {
+    const { selected } = this.state
+    const selectedItems = selected.slice()
+    // const node = this.nodes.get(i)
+    // console.log(node)
+    const index = selected.indexOf(id)
+    if (index === -1) {
+      if (e.shiftKey) {
+        this.setState(prevState => ({
+          selected: [...prevState.selected, id],
+        }))
+        return
+      }
+      this.setState({
+        selected: [id],
+      })
+    } else {
+      if (e.shiftKey) {
+        selectedItems.splice(index, 1)
+        this.setState({
+          selected: selectedItems,
+        })
+        return
+      }
+      this.setState({
+        selected: [],
+      })
+    }
+  }
+
   render() {
+    const { selected } = this.state
     const { activeSong, songs, title, playlist, songState } = this.props
 
     return (
@@ -56,36 +122,39 @@ class SongsTable extends React.Component<Props, State> {
         <table cellSpacing="0">
           <thead>
             <tr>
+              <th />
               <th>Title</th>
               <th>Time</th>
               <th>Artist</th>
               <th>Album</th>
               <th>Genre</th>
+              <th />
             </tr>
           </thead>
           <tbody>
             {songs.map((song, index) => (
               <tr
                 key={song.id}
+                ref={c => {
+                  this.nodes.set(index, c)
+                }}
+                onClick={e => this.selectRow(e, song.id)}
                 onDoubleClick={() => this.playAtIndex(index)}
-                className={song.id === activeSong ? `${table.active_song}` : ''}
+                onContextMenu={e => this.onContextMenu(song, e)}
+                className={`${song.id === activeSong ? `${table.active_song}` : ''} ${
+                  selected.includes(song.id) ? `${table.selected}` : ''
+                }`}
               >
-                <td>{song.meta.name || song.filename}</td>
-                <td>{song.duration ? humanizeDuration(song.duration) : ''}</td>
-                <td>{song.meta.artists_original || 'Unknown'}</td>
-                <td>{song.meta.album || 'Unknown'}</td>
-                <td>{song.meta.genre || 'Unknown'} </td>
                 {song.id === activeSong ? (
-                  <td className={`${table.playingSongIcon} ${flex.align_center} ${flex.space_between}`}>
+                  <td className={`${table.playingSongIcon}`}>
                     <button type="button" className={`${button.btn} ${button.btn_blue}`} onClick={() => this.playPause()}>
                       <i title={songState === 'playing' ? 'Pause' : 'Play'} className="material-icons">
                         {songState === 'playing' ? 'pause' : 'play_arrow'}
                       </i>
                     </button>
-                    <SongDropdown song={song} playlist={playlist} />
                   </td>
                 ) : (
-                  <td className={`${table.song_wrapper_btns} ${flex.space_between}`}>
+                  <td className={selected.includes(song.id) ? `${table.selected_row}` : `${table.hover_btns}`}>
                     <button
                       type="button"
                       onClick={() => this.playAtIndex(index)}
@@ -93,9 +162,16 @@ class SongsTable extends React.Component<Props, State> {
                     >
                       <i className="material-icons">play_arrow</i>
                     </button>
-                    <SongDropdown song={song} playlist={playlist} />
                   </td>
                 )}
+                <td>{song.meta.name || song.filename}</td>
+                <td>{song.duration ? humanizeDuration(song.duration) : ''}</td>
+                <td>{song.meta.artists_original || 'Unknown'}</td>
+                <td>{song.meta.album || 'Unknown'}</td>
+                <td>{song.meta.genre || 'Unknown'} </td>
+                <td className={selected.includes(song.id) ? `${table.selected_row}` : `${table.hover_btns}`}>
+                  <SongDropdown song={song} playlist={playlist} />
+                </td>
               </tr>
             ))}
           </tbody>
