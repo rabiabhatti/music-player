fc271656b132746e5e81d8f9c587620e57ff44d5
Steel Brain <i@steelbrain.me>
2019-01-12 01:52:50 +0500

    :fire: Cleanup AddMusic

--- src/components/AddMusic.js
+++ src/components/AddMusic.js
@@ -1,10 +1,8 @@
 // @flow
 
 import React from 'react'
-import { compose } from 'recompose'
 
 import db from '~/db'
-import { incrementNonce } from '~/redux/songs'
 import type { UserAuthorization } from '~/redux/user'
 
 import services from '~/services'
@@ -13,103 +11,59 @@ import connect from '~/common/connect'
 import flex from '~/styles/flex.less'
 import button from '~/styles/button.less'
 import addMusic from '~/styles/addMusic.less'
-import googleDriveIcon from '~/static/img/google_drive_icon.png'
 import HeaderDropdown from '~/components/Dropdown/HeaderDropdown'
 
 type Props = {|
-  nonce: number,
-  incrementNonce: () => void,
   authorizations: Array<UserAuthorization>,
 |}
-type State = {|
-  handleClose: boolean,
-  authorizationServices: Array<UserAuthorization>,
-|}
-
-class AddMusic extends React.Component<Props, State> {
-  state = { authorizationServices: [], handleClose: false }
-
-  componentDidMount() {
-    this.loadServices()
-  }
-  componentDidUpdate(prevProps) {
-    const { nonce } = this.props
-    if (prevProps.nonce !== nonce) this.loadServices()
-  }
-
-  loadServices = () => {
-    const { authorizations } = this.props
-    if (!authorizations.length) {
-      console.warn('No authorizations found')
-      return
-    }
-
-    authorizations.map(authorization => {
-      const { authorizationServices: authorizationServicesState } = this.state
-      return this.setState({ authorizationServices: [...authorizationServicesState, authorization] })
-    })
-  }
-
-  createPicker = (e: SyntheticEvent<HTMLButtonElement>) => {
+class AddMusic extends React.PureComponent<Props> {
+  createPicker = (e: SyntheticEvent<HTMLButtonElement>, authorization: UserAuthorization) => {
     e.preventDefault()
-    const { authorizations } = this.props
-    if (!authorizations.length) {
-      console.warn('No authorizations found')
+
+    const service = services.find(item => item.name === authorization.service)
+    if (!service) {
+      console.warn('Service not found for authorization', authorization)
       return
     }
-    authorizations.forEach(authorization => {
-      const { incrementNonce: incrementNonceProp } = this.props
-      const service = services.find(item => item.name === authorization.service)
-      if (!service) {
-        console.warn('Service not found for authorization', authorization)
-        return
-      }
 
-      service
-        .addFiles(authorization)
-        .then(filesChosen => {
-          db.songs.bulkAdd(filesChosen)
-          incrementNonceProp()
-        })
-        .catch(console.error)
-    })
-    this.setState({
-      handleClose: true,
-    })
-  }
-
-  addService(name: string, img: string, alt: string) {
-    const { authorizationServices } = this.state
-    const auth = authorizationServices.find(item => item.service === name)
-
-    return (
-      auth && (
-        <button title={name} className={`${button.btn} ${flex.justify_start}`} type="button" onClick={this.createPicker}>
-          <img className={`${addMusic.serviceImg}`} src={img} alt={alt} />
-          <div className={`${flex.column} ${flex.align_start}`}>
-            <span className={`${addMusic.service_user_name}`}>{auth.user_name}</span>
-            <span className={`${addMusic.service_email}`}>{auth.email}</span>
-          </div>
-        </button>
-      )
-    )
+    service
+      .addFiles(authorization)
+      .then(filesChosen => {
+        db.songs.bulkAdd(filesChosen)
+      })
+      .catch(console.error)
   }
 
   render() {
-    const { handleClose } = this.state
+    const { authorizations } = this.props
 
-    return (
-      <HeaderDropdown handleClose={handleClose} buttonTitle="Add Music" buttonIcon="add" className={`${addMusic.add_music}`}>
+    return authorizations.length ? (
+      <HeaderDropdown buttonTitle="Add Music" buttonIcon="add" className={`${addMusic.add_music}`}>
         <h3>Choose Service</h3>
-        {this.addService('GoogleDrive', googleDriveIcon, 'google_drive_icon')}
+        {authorizations.map(authorization => {
+          const service = services.find(item => item.name === authorization.service)
+          if (!service) {
+            return null
+          }
+
+          return (
+            <button
+              key={`${authorization.uid}-${authorization.service}`}
+              className={`${button.btn} ${flex.justify_start}`}
+              type="button"
+              onClick={e => this.createPicker(e, authorization)}
+            >
+              <img className={`${addMusic.serviceImg}`} src={service.thumbnail} alt={service.name} />
+              <div className={`${flex.column} ${flex.align_start}`}>
+                <span className={`${addMusic.service_user_name}`}>{authorization.user_name}</span>
+                <span className={`${addMusic.service_email}`}>{authorization.email}</span>
+              </div>
+            </button>
+          )
+        })}
       </HeaderDropdown>
-    )
+    ) : null
   }
 }
 
-export default compose(
-  connect(
-    state => ({ authorizations: state.user.authorizations.toArray(), nonce: state.songs.nonce }),
-    { incrementNonce },
-  ),
-)(AddMusic)
+export default connect(state => ({ authorizations: state.user.authorizations.toArray() }))(AddMusic)
