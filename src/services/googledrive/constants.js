b54f6f979dfe20f8f966cbf5838e3f1edce67e20
Rabia Bhatti <rabia.iqbal.bhatti@gmail.com>
2019-06-30 23:05:59 +0500

    Change scope

--- src/services/googledrive/constants.js
+++ src/services/googledrive/constants.js
@@ -4,7 +4,7 @@ const APP_ID = process.env.APP_ID
 const API_KEY = process.env.API_KEY
 const CLIENT_ID = process.env.CLIENT_ID
 
-const SCOPES = 'https://www.googleapis.com/auth/drive.readonly'
+const SCOPES = 'https://www.googleapis.com/auth/drive.file'
 const DISCOVERY_DOCS = ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest']
 
 export { APP_ID, API_KEY, CLIENT_ID, SCOPES, DISCOVERY_DOCS }
